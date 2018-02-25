8b2a91eb1b2388509273083539914c690ea5e203
Brandon Semilla <semibran@gmail.com>
2018-02-25 01:36:49 -0800

    remove out of bounds actors

--- src/render.js
+++ src/render.js
@@ -7,16 +7,23 @@ export default (h, { world, viewport }, actions) =>
         height: viewport.size[1] + "px"
       })
     },
-    world.actors.map(actor => {
-      let [ hw, hh ] = actor.hitbox.halfsize
+    world.actors.reduce((elements, actor) => {
+      let [ halfwidth, halfheight ] = actor.hitbox.halfsize
       let [ x, y ] = actor.hitbox.position
-      return h("div", { class: "actor",
-        style: stringify({
-          width: hw * 2 + "px",
-          height: hh * 2 + "px",
-          left: x - hw + "px",
-          top: y - hh + "px"
-        })
-      })
-    })
+      if (x + halfwidth >= 0 && x - halfwidth < viewport.size[0]
+      &&  y + halfheight >= 0 && y - halfheight < viewport.size[1]
+      ) {
+        elements.push(
+          h("div", { class: "actor",
+            style: stringify({
+            width: halfwidth * 2 + "px",
+            height: halfheight * 2 + "px",
+            left: x - halfwidth + "px",
+            top: y - halfheight + "px"
+            })
+          })
+        )
+      }
+      return elements
+    }, [])
   )
